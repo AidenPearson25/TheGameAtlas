e078475e3335262dacc705efcd03f577dceadb4c
naranjmr <naranjmr@miamioh.edu>
2023-10-05 14:24:08 -0400

    I got in - Mario

--- src/Testing.java
+++ src/Testing.java
@@ -3,5 +3,6 @@ public class Testing {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("I am become dev, greeters of world");
+		System.out.println("Mario has entered the chat");
 	}
 }
