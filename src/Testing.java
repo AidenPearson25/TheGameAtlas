1dc77579d219a721f99a870da7bc9b6bf429abf9
AidenPearson25 <pearsoa6@miamioh.edu>
2023-10-10 13:53:54 -0400

    Dummy project built
    Basic hello world file so we can show how our program will run

--- src/Testing.java
+++
@@ -1,9 +0,0 @@
-
-public class Testing {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("I am become dev, greeters of world");
-		System.out.println("If the C is sharp, why can't I weaponize it?");
-		System.out.println("Mario has entered the chat");
-	}
-}
