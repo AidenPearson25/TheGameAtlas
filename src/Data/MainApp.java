d99d67d796c3df4473b4d29d7bd0f22c04b659f8
naranjmr <naranjmr@miamioh.edu>
2023-10-24 22:21:48 -0400

    Finished making Account Page, Login, and Create Account

--- src/Data/MainApp.java
+++ src/Data/MainApp.java
@@ -1,4 +1,5 @@
 package Data;
+
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -23,151 +24,171 @@ import Display.AccountPage;
 import Display.ContentPage;
 
 public class MainApp extends JFrame {
-	
-	private static final long serialVersionUID = 1L;
-	//Main pane of the frame
-	private JPanel contentPane;
-	
-	//The current jpanel that's active
-	static JPanel currentPage;
-	
-	//The current jscrollpane that's active (Might have to edit if more things require scrolls later)
-	JScrollPane scroll;
-	
-	// The JButton needed for the account page
-	private JButton accountButton;
-	
-	/**
-	 * Main, sets up and runs program.
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MainApp frame = new MainApp();
-					frame.setVisible(true);
-					frame.setTitle("The Game Atlas");
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-	
-	/**
-	 * Constructor, makes the JFrame.
-	 */
-	public MainApp() {
-		//Initial Setup
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1051, 531);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(new CardLayout(0, 0));
-		
-		//Setup for mainPage JPanel
-		SearchPage mainPage = new SearchPage("searchPage");
-		currentPage = mainPage.DisplayPage(contentPane); //Display page adds it to content pane
-		
-		//Read game data from text doc
-		ArrayList<Game> games = readGameData("GameDatabase.txt");
-		
-		//Setup scroll 
-		int ySize = (games.size() / 6) + 1; //Sets vertical scroll length
-		mainPage.GetRef().setPreferredSize(new Dimension(1051, ySize * 150));
-		
-		scroll = new JScrollPane(mainPage.GetRef());
-		scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		scroll.getVerticalScrollBar().setUnitIncrement(8);
-		contentPane.add(scroll);
-		
-		// Add an "Account button
-    AccountPage accountPage = new AccountPage("accountPage");
-    accountButton = new JButton("Account");
-    accountButton.addActionListener(e -> ChangeActivePanel(accountPage.GetRef()));
-    mainPage.GetRef().add(accountButton); //Make sure to add to mainPage instead of contentPane
-		
-        
-		//Create thumbnails, content pages, and button actions
-		for (Game g : games) {
-			//Create thumbnail
-			Thumbnail thumbnail = new Thumbnail(g.GetName());
-			mainPage.GetRef().add(thumbnail.Display(g)); //Will have to add GetRef to get the JPane;
-			
-			//Create content page
-			ContentPage contentPage = new ContentPage(g.GetName());
-			contentPage.DisplayPage(contentPane);
-			contentPage.AddData(g, thumbnail);
-			
-			//Action Listeners will run when buttons are pushed
-			thumbnail.GetButton().addActionListener(e -> ChangeActivePanel(contentPage.GetRef()));
-			contentPage.GetBackButton().addActionListener(e -> ChangeActivePanel(mainPage.GetRef(), scroll));
-		}
-		
-		
-		
-	}
-	
-	/**
-	 * Change the active panel
-	 * @param current
-	 */
-	void ChangeActivePanel(JPanel current) {
-	    scroll.setVisible(false);
-	    current.setVisible(true);
-	    MainApp.currentPage = current;
-	}
-	
-	/**
-	 * Change the active panel with scroll check (Might have to edit later)
-	 * @param current
-	 * @param currentScroll
-	 */
-	void ChangeActivePanel(JPanel current, JScrollPane currentScroll) {
-	    MainApp.currentPage.setVisible(false);
-	    current.setVisible(true);
-	    currentScroll.setVisible(true);
-	    MainApp.currentPage = current;
-	}
-	
-	/**
-	 * Reads the game data from a text doc, creates game objects, returns array.
-	 * @param filename
-	 * @return
-	 */
-	private static ArrayList<Game> readGameData(String filename) {
-		ArrayList<Game> games = new ArrayList<>();
-		int i = 0; //Detlete later
-		
-		//Scanner read
-		while(i < 7) { //Delete later, pads for scroll testing
-		try {
-			Scanner readGames = new Scanner(new File(filename));
-			while (true) { //Trust me on this
-				Game current = new Game(readGames.nextLine());
-				current.SetDescription(readGames.nextLine());
-				current.SetIconRef(readGames.nextLine());
-				current.SetGenre(readGames.nextLine());
-				current.SetPrice(new double[] {0.0}); //Add later
-				current.SetPlatforms(new boolean[] {false}); //Add later
-				readGames.nextLine(); //Delete later
-				readGames.nextLine(); //Delete later
-				games.add(current);
-				
-				if (!readGames.hasNextLine()) {
-					break;
-				}
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		i++; // Delete later
-		}
-		
-		return games;
-	}
-	
-	
+
+    private static final long serialVersionUID = 1L;
+    // Main pane of the frame
+    private JPanel contentPane;
+
+    // The current jpanel that's active
+    static JPanel currentPage;
+
+    // The current jscrollpane that's active (Might have to edit if more things
+    // require scrolls later)
+    JScrollPane scroll;
+
+    // The JButton needed for the account page
+    private JButton accountButton;
+
+    /**
+     * Main, sets up and runs program.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    MainApp frame = new MainApp();
+                    frame.setVisible(true);
+                    frame.setTitle("The Game Atlas");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Constructor, makes the JFrame.
+     */
+    public MainApp() {
+        // Initial Setup
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 1051, 531);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(new CardLayout(0, 0));
+
+        // Setup for mainPage JPanel
+        SearchPage mainPage = new SearchPage("searchPage");
+        currentPage = mainPage.DisplayPage(contentPane); // Display page adds it
+                                                         // to content pane
+
+        // Read game data from text doc
+        ArrayList<Game> games = readGameData("GameDatabase.txt");
+
+        // Setup scroll
+        int ySize = (games.size() / 6) + 1; // Sets vertical scroll length
+        mainPage.GetRef().setPreferredSize(new Dimension(1051, ySize * 150));
+
+        scroll = new JScrollPane(mainPage.GetRef());
+        scroll.setHorizontalScrollBarPolicy(
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scroll.getVerticalScrollBar().setUnitIncrement(8);
+        contentPane.add(scroll);
+
+        // Add an "Account button
+        AccountPage accountPage = new AccountPage("accountPage");
+        accountButton = new JButton("Account"); // Creates a button that leads
+                                                // to the Account page
+        accountButton.addActionListener(
+                e -> ChangeActivePanel(accountPage.GetRef()));
+        mainPage.GetRef().add(accountButton); // Make sure to add to mainPage
+                                              // instead of contentPane
+        accountPage.DisplayPage(contentPane); // Displays the accountPage
+        accountPage.initialize(); // Formats the accountPage
+        accountPage.GetBackButton().addActionListener(
+                e -> ChangeActivePanel(mainPage.GetRef(), scroll)); // Makes the
+                                                                    // back
+                                                                    // button
+                                                                    // lead back
+                                                                    // to the
+                                                                    // main
+                                                                    // page.
+
+        // Create thumbnails, content pages, and button actions
+        for (Game g : games) {
+            // Create thumbnail
+            Thumbnail thumbnail = new Thumbnail(g.GetName());
+            mainPage.GetRef().add(thumbnail.Display(g)); // Will have to add
+                                                         // GetRef to get the
+                                                         // JPane;
+
+            // Create content page
+            ContentPage contentPage = new ContentPage(g.GetName());
+            contentPage.DisplayPage(contentPane);
+            contentPage.AddData(g, thumbnail);
+
+            // Action Listeners will run when buttons are pushed
+            thumbnail.GetButton().addActionListener(
+                    e -> ChangeActivePanel(contentPage.GetRef()));
+            contentPage.GetBackButton().addActionListener(
+                    e -> ChangeActivePanel(mainPage.GetRef(), scroll));
+        }
+
+    }
+
+    /**
+     * Change the active panel
+     * 
+     * @param current
+     */
+    void ChangeActivePanel(JPanel current) {
+        scroll.setVisible(false);
+        current.setVisible(true);
+        MainApp.currentPage = current;
+    }
+
+    /**
+     * Change the active panel with scroll check (Might have to edit later)
+     * 
+     * @param current
+     * @param currentScroll
+     */
+    void ChangeActivePanel(JPanel current, JScrollPane currentScroll) {
+        MainApp.currentPage.setVisible(false);
+        current.setVisible(true);
+        currentScroll.setVisible(true);
+        MainApp.currentPage = current;
+    }
+
+    /**
+     * Reads the game data from a text doc, creates game objects, returns array.
+     * 
+     * @param filename
+     * @return
+     */
+    private static ArrayList<Game> readGameData(String filename) {
+        ArrayList<Game> games = new ArrayList<>();
+        int i = 0; // Detlete later
+
+        // Scanner read
+        while (i < 7) { // Delete later, pads for scroll testing
+            try {
+                Scanner readGames = new Scanner(new File(filename));
+                while (true) { // Trust me on this
+                    Game current = new Game(readGames.nextLine());
+                    current.SetDescription(readGames.nextLine());
+                    current.SetIconRef(readGames.nextLine());
+                    current.SetGenre(readGames.nextLine());
+                    current.SetPrice(new double[] { 0.0 }); // Add later
+                    current.SetPlatforms(new boolean[] { false }); // Add later
+                    readGames.nextLine(); // Delete later
+                    readGames.nextLine(); // Delete later
+                    games.add(current);
+
+                    if (!readGames.hasNextLine()) {
+                        break;
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            i++; // Delete later
+        }
+
+        return games;
+    }
+
 }
--- src/Display/AccountPage.java
+++ src/Display/AccountPage.java
@@ -1,11 +1,216 @@
 package Display;
 
-public class AccountPage extends Page{
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AccountPage extends Page {
+
+    // These are the buttons for the page
+    JButton backButton;
+    JButton login;
+    JButton create;
+
+    // These is the text on the page
+    JLabel userLabel;
+    JLabel passwordLabel;
+
+    // These are the text boxes in the page
+    JTextField userText;
+    JTextField passwordText;
+
+    private Map<String, String> userDatabase = new HashMap<>(); // In-memory
+                                                                // user storage
 
     public AccountPage(String name) {
         super(name);
+        loadUserData(); // This method loads all usernames and passwords made
+                        // beforehand.
     }
-    
-    
 
+    /**
+     * This method basically sets up the page. The GridBagLayout is a way of
+     * formatting all the elements in the page.
+     */
+    public void initialize() {
+        // Create a panel with GridBagLayout
+        JPanel panelWithLayout = new JPanel(new GridBagLayout());
+
+        // Create GridBagConstraints for component placement
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets.left = 10;
+        gbc.insets.top = 10;
+
+        // User Label
+        userLabel = new JLabel("Username");
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        panelWithLayout.add(userLabel, gbc);
+
+        // User Text
+        userText = new JTextField(20);
+        gbc.gridx = 1;
+        panelWithLayout.add(userText, gbc);
+
+        // Password Label
+        passwordLabel = new JLabel("Password");
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        panelWithLayout.add(passwordLabel, gbc);
+
+        // Password Text
+        passwordText = new JTextField(20);
+        gbc.gridx = 1;
+        panelWithLayout.add(passwordText, gbc);
+
+        // Back Button
+        backButton = new JButton("Back");
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        gbc.insets.top = 20;
+        panelWithLayout.add(backButton, gbc);
+
+        // Login Button
+        login = new JButton("Login");
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        gbc.insets.top = 20;
+        panelWithLayout.add(login, gbc);
+
+        // This calls the checkLogin() when the button is clicked.
+        login.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Call the checkLogin method when the login button is clicked
+                checkLogin();
+            }
+        });
+
+        // Create Account Button
+        create = new JButton("Create Account");
+        gbc.gridx = 2;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        gbc.insets.top = 20;
+        panelWithLayout.add(create, gbc);
+
+        // This calls the createAccount() when the create account button is
+        // clicked.
+        create.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Call the checkLogin method when the login button is clicked
+                createAccount();
+            }
+        });
+
+        // Add the panel with GridBagLayout to the main panel
+        panel.add(panelWithLayout);
+    }
+
+    /**
+     * This method loads all the previous user data from a text file called,
+     * "user_data.txt"
+     */
+    public void loadUserData() {
+        // Read the user data from a text file
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new FileReader("user_data.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(":");
+                if (parts.length == 2) {
+                    userDatabase.put(parts[0], parts[1]);
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            // File doesn't exist or cannot be read, so continue with an empty
+            // userDatabase.
+        }
+    }
+
+    /**
+     * This method saves the username and password made in createAccount()
+     */
+    public void saveUserData() {
+        try {
+            PrintWriter writer = new PrintWriter(
+                    new FileWriter("user_data.txt"));
+            for (Map.Entry<String, String> entry : userDatabase.entrySet()) {
+                writer.println(entry.getKey() + ":" + entry.getValue());
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method checks the username and password typed in the text boxes with
+     * the textfile to see if the user has an account to login with.
+     */
+    public void checkLogin() {
+        // Capture the entered username and password
+        String enteredUsername = userText.getText();
+        String enteredPassword = passwordText.getText();
+
+        // Check if the entered username exists and the password matches
+        String storedPassword = userDatabase.get(enteredUsername);
+
+        if (storedPassword != null && storedPassword.equals(enteredPassword)) {
+            // Successful login
+            JOptionPane.showMessageDialog(null, "Login successful!");
+        } else {
+            // Failed login
+            JOptionPane.showMessageDialog(null,
+                    "Login failed. Please check your credentials.");
+        }
+    }
+
+    /**
+     * This method takes the username and password typed in the text boxes and
+     * checks if the same username is in the textfile. If not, then a new
+     * account is created and saved.
+     */
+    public void createAccount() {
+        // Capture the entered username and password
+        String enteredUsername = userText.getText();
+        String enteredPassword = passwordText.getText();
+
+        if (userDatabase.containsKey(enteredUsername)) {
+            JOptionPane.showMessageDialog(null,
+                    "Username is already in use. Please choose another option.");
+            return;
+        }
+
+        userDatabase.put(enteredUsername, enteredPassword);
+        JOptionPane.showMessageDialog(null,
+                "Account created successfully. Please Login");
+
+        saveUserData();
+
+    }
+
+    // Gets a reference to the back button for action listener
+    public JButton GetBackButton() {
+        return backButton;
+    }
 }
