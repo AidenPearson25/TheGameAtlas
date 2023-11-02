7d5db713d7266e11e8ff2b33e40a380fe26a83ae
AidenPearson25 <pearsoa6@miamioh.edu>
2023-11-01 22:34:38 -0400

    Cleanup and TDD

--- src/TDD/GameThumbnailTDD.java
+++ src/TDD/GameThumbnailTDD.java
@@ -5,23 +5,26 @@ import org.junit.jupiter.api.Test;
 import static org.junit.Assert.*;
 
 import java.util.Scanner;
+import java.awt.Dimension;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 import Data.Game;
+import Data.Thumbnail;
 
-public class GameTDD {
+public class GameThumbnailTDD {
 	final String DESCRIPTION = "Forge your own path in Hollow Knight!" +
 														 " An epic action adventure through a vast" +
 														 " ruined kingdom of insects and heroes. Explore" +
 														 " twisting caverns, battle tainted creatures and" +
 														 " befriend bizarre bugs, all in a classic," +
 														 " hand-drawn 2D style.";
+	Game game;
 	
-	@Test
-	public void testReadFromDatabase() throws FileNotFoundException {
+	@BeforeEach
+	public void Setup() throws FileNotFoundException {
 		Scanner read = new Scanner(new File("GameDatabase.txt"));
-		Game game = new Game(read.nextLine());
+		game = new Game(read.nextLine());
 		game.SetDescription(read.nextLine());
     game.SetIconRef(read.nextLine());
     game.SetGenre(read.nextLine());
@@ -32,7 +35,10 @@ public class GameTDD {
 		for (int i = 0; i < 3; i++) {
       game.SetPlatforms(temp.nextBoolean(), i);
 		}
-		
+	}
+	
+	@Test
+	public void testReadGameDatabase(){
 		assertEquals(game.GetName(), "Hollow Knight");
 		assertEquals(game.GetDescription(), DESCRIPTION);
 		assertEquals(game.GetIconRef(), "iconImages/hollowknight.jpg");
@@ -41,5 +47,13 @@ public class GameTDD {
 		assertEquals(game.GetPlatform(0), true);
 		assertEquals(game.GetPlatform(1), true);
 		assertEquals(game.GetPlatform(2), true);
+	}
+	
+	@Test
+	public void testThumbnail() {
+		Thumbnail thumbnail = new Thumbnail(game);
+    thumbnail.Display();
+
+    assertEquals(thumbnail.GetButton().getPreferredSize(), new Dimension(150, 150));
 	}
 }
