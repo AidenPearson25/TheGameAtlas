9f89661a6d99a662c29cbc2057194c3e0a949425
AidenPearson25 <pearsoa6@miamioh.edu>
2023-11-30 13:06:14 -0500

    Forget what I added

--- src/Data/MainApp.java
+++ src/Data/MainApp.java
@@ -57,7 +57,7 @@ public class MainApp extends JFrame {
      * 
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) {	
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
